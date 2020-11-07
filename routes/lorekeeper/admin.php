fa50e3d91c2d5870c5167b2bcb0267460ab4b3db
Toffee-tama <65995905+Toffee-tama@users.noreply.github.com>
2020-11-07 22:06:08 +0800

    no message

--- routes/lorekeeper/admin.php
+++ routes/lorekeeper/admin.php
@@ -74,16 +74,6 @@ Route::group(['prefix' => 'images', 'middleware' => 'power:edit_site_settings'],
 
 # DATA
 Route::group(['prefix' => 'data', 'namespace' => 'Data', 'middleware' => 'power:edit_data'], function() {
-    
-    # GALLERIES
-    Route::get('galleries', 'GalleryController@getIndex');
-    Route::get('galleries/create', 'GalleryController@getCreateGallery');
-    Route::get('galleries/edit/{id}', 'GalleryController@getEditGallery');
-    Route::get('galleries/delete/{id}', 'GalleryController@getDeleteGallery');
-    Route::post('galleries/create', 'GalleryController@postCreateEditGallery');
-    Route::post('galleries/edit/{id?}', 'GalleryController@postCreateEditGallery');
-    Route::post('galleries/delete/{id}', 'GalleryController@postDeleteGallery');
-    Route::post('galleries/sort', 'GalleryController@postSortGallery');
 
     # CURRENCIES
     Route::get('currencies', 'CurrencyController@getIndex');
@@ -215,22 +205,6 @@ Route::group(['prefix' => 'data', 'namespace' => 'Data', 'middleware' => 'power:
     Route::post('prompts/create', 'PromptController@postCreateEditPrompt');
     Route::post('prompts/edit/{id?}', 'PromptController@postCreateEditPrompt');
     Route::post('prompts/delete/{id}', 'PromptController@postDeletePrompt');
-    
-    # SCAVENGER HUNTS
-    Route::get('hunts', 'HuntController@getHuntIndex');
-    Route::get('hunts/create', 'HuntController@getCreateHunt');
-    Route::get('hunts/edit/{id}', 'HuntController@getEditHunt');
-    Route::get('hunts/delete/{id}', 'HuntController@getDeleteHunt');
-    Route::post('hunts/create', 'HuntController@postCreateEditHunt');
-    Route::post('hunts/edit/{id?}', 'HuntController@postCreateEditHunt');
-    Route::post('hunts/delete/{id}', 'HuntController@postDeleteHunt');
-
-    Route::get('hunts/targets/create/{id}', 'HuntController@getCreateHuntTarget');
-    Route::post('hunts/targets/create', 'HuntController@postCreateEditHuntTarget');
-    Route::get('hunts/targets/edit/{id}', 'HuntController@getEditHuntTarget');
-    Route::post('hunts/targets/edit/{id}', 'HuntController@postCreateEditHuntTarget');
-    Route::get('hunts/targets/delete/{id}', 'HuntController@getDeleteHuntTarget');
-    Route::post('hunts/targets/delete/{id}', 'HuntController@postDeleteHuntTarget');
 });
 
 
@@ -411,15 +385,6 @@ Route::group(['prefix' => 'claims', 'middleware' => 'power:manage_submissions'],
     Route::get('/{status}', 'SubmissionController@getClaimIndex')->where('status', 'pending|approved|rejected');
     Route::get('edit/{id}', 'SubmissionController@getClaim');
     Route::post('edit/{id}/{action}', 'SubmissionController@postSubmission')->where('action', 'approve|reject');
-});
-
-# SUBMISSIONS
-Route::group(['prefix' => 'gallery', 'middleware' => 'power:manage_submissions'], function() {
-    Route::get('/submissions', 'GalleryController@getSubmissionIndex');
-    Route::get('/submissions/{status}', 'GalleryController@getSubmissionIndex')->where('status', 'pending|accepted|rejected');
-    Route::get('/currency', 'GalleryController@getCurrencyIndex');
-    Route::get('/currency/{status}', 'GalleryController@getCurrencyIndex')->where('status', 'pending|valued');
-    Route::post('edit/{id}/{action}', 'GalleryController@postEditSubmission')->where('action', 'accept|reject|comment|move|value');
 });
 
 # REPORTS
