0d62a610af8364add9dc76bea6d471b56fb413b0
Toffee-tama <65995905+Toffee-tama@users.noreply.github.com>
2020-11-08 09:45:56 +0800

    no message

--- routes/lorekeeper/admin.php
+++ routes/lorekeeper/admin.php
@@ -420,6 +420,7 @@ Route::group(['prefix' => 'gallery', 'middleware' => 'power:manage_submissions']
     Route::get('/currency', 'GalleryController@getCurrencyIndex');
     Route::get('/currency/{status}', 'GalleryController@getCurrencyIndex')->where('status', 'pending|valued');
     Route::post('edit/{id}/{action}', 'GalleryController@postEditSubmission')->where('action', 'accept|reject|comment|move|value');
+
 # REPORTS
 Route::group(['prefix' => 'reports', 'middleware' => 'power:manage_reports'], function() {
     Route::get('/', 'ReportController@getReportIndex');
@@ -434,4 +435,3 @@ Route::group(['prefix' => 'designs', 'middleware' => 'power:manage_characters'],
     Route::post('edit/{id}/{action}', 'DesignController@postDesign')->where('action', 'cancel|approve|reject');
 });
 Route::get('{type}/{status}', 'DesignController@getDesignIndex')->where('type', 'myo-approvals|design-approvals')->where('status', 'pending|approved|rejected');
-});
