32f9e7a933b13534edd36ee3f1062678c60ae4b1
Toffee-tama <65995905+Toffee-tama@users.noreply.github.com>
2020-11-08 10:36:05 +0800

    trying to fix routes

--- routes/lorekeeper/admin.php
+++ routes/lorekeeper/admin.php
@@ -215,6 +215,7 @@ Route::group(['prefix' => 'data', 'namespace' => 'Data', 'middleware' => 'power:
     Route::post('prompts/create', 'PromptController@postCreateEditPrompt');
     Route::post('prompts/edit/{id?}', 'PromptController@postCreateEditPrompt');
     Route::post('prompts/delete/{id}', 'PromptController@postDeletePrompt');
+    Route::post('prompts/delete/{id}', 'PromptController@postDeletePrompt');
 
     # SCAVENGER HUNTS
     Route::get('hunts', 'HuntController@getHuntIndex');
@@ -420,6 +421,7 @@ Route::group(['prefix' => 'gallery', 'middleware' => 'power:manage_submissions']
     Route::get('/currency', 'GalleryController@getCurrencyIndex');
     Route::get('/currency/{status}', 'GalleryController@getCurrencyIndex')->where('status', 'pending|valued');
     Route::post('edit/{id}/{action}', 'GalleryController@postEditSubmission')->where('action', 'accept|reject|comment|move|value');
+});
 
 # REPORTS
 Route::group(['prefix' => 'reports', 'middleware' => 'power:manage_reports'], function() {
@@ -433,7 +435,6 @@ Route::group(['prefix' => 'reports', 'middleware' => 'power:manage_reports'], fu
 Route::group(['prefix' => 'designs', 'middleware' => 'power:manage_characters'], function() {
     Route::get('edit/{id}/{action}', 'DesignController@getDesignConfirmation')->where('action', 'cancel|approve|reject');
     Route::post('edit/{id}/{action}', 'DesignController@postDesign')->where('action', 'cancel|approve|reject');
-    Route::post('vote/{id}/{action}', 'DesignController@postVote')->where('action', 'approve|reject');
 });
 Route::get('{type}/{status}', 'DesignController@getDesignIndex')->where('type', 'myo-approvals|design-approvals')->where('status', 'pending|approved|rejected');
 });
