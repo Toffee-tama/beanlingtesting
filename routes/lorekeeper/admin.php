c52cf4b1c3e972c5df90bce2ebfbf1292e704b30
Toffee-tama <65995905+Toffee-tama@users.noreply.github.com>
2020-11-08 10:18:35 +0800

    fixed <<< error

--- routes/lorekeeper/admin.php
+++ routes/lorekeeper/admin.php
@@ -420,10 +420,6 @@ Route::group(['prefix' => 'gallery', 'middleware' => 'power:manage_submissions']
     Route::get('/currency', 'GalleryController@getCurrencyIndex');
     Route::get('/currency/{status}', 'GalleryController@getCurrencyIndex')->where('status', 'pending|valued');
     Route::post('edit/{id}/{action}', 'GalleryController@postEditSubmission')->where('action', 'accept|reject|comment|move|value');
-<<<<<<< HEAD
-=======
-
->>>>>>> 69f227fdd64570f87e25d766416175b8ba45d795
 # REPORTS
 Route::group(['prefix' => 'reports', 'middleware' => 'power:manage_reports'], function() {
     Route::get('/', 'ReportController@getReportIndex');
