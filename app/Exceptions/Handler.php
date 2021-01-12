b05b8a7ab496493397c263a459bf5a052653472b
Toffee-tama <65995905+Toffee-tama@users.noreply.github.com>
2021-01-12 15:21:15 +0800

    trying to fix login issue

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -51,13 +51,8 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return \Illuminate\Http\Response
      */
-public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)
     {
-    if ($exception instanceof Illuminate\Session\TokenMismatchException) {
-        Artisan::call('cache:clear');
-        Artisan::call('config:cache');
-        return Redirect::back();
-    }
-    return parent::render($request, $exception);
+        return parent::render($request, $exception);
     }
 }
