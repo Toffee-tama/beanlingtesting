79532564796ec3c78027e3cbf3fd391e6d59d164
Toffee-tama <65995905+Toffee-tama@users.noreply.github.com>
2021-02-12 16:17:25 +0800

    no message

--- resources/views/admin/items/tags/box_post.blade.php
+++ resources/views/admin/items/tags/box_post.blade.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-@include('widgets._loot_select_row', ['items' => $items, 'currencies' => $currencies, 'awards' => $awards, 'showLootTables' => true, 'showRaffles' => true])
-=======
-@include('widgets._loot_select_row', ['items' => $items, 'currencies' => $currencies, 'showLootTables' => true, 'showRaffles' => true, 'showRecipes' => true])
->>>>>>> d6c2d037946decba62b3720114d4ce721774e437
+@include('widgets._loot_select_row', ['items' => $items, 'currencies' => $currencies, 'awards' => $awards, 'showLootTables' => true, 'showRaffles' => true, 'showRecipes' => true])
