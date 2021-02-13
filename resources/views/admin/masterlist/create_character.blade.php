865b9e57b3a131a61e980cae8137d67f5bbb4f81
Toffee-tama <65995905+Toffee-tama@users.noreply.github.com>
2021-02-13 16:50:33 +0800

    removed paramater info

--- resources/views/admin/masterlist/create_character.blade.php
+++ resources/views/admin/masterlist/create_character.blade.php
@@ -302,7 +302,6 @@
     <div class="card mb-3 hide" id="dropOptions">
         <div class="card-body" id="groups">
             {!! Form::label('Group (Optional)') !!} {!! add_help('This is used for character drops. If no value is set, it will be randomly rolled from the species\' groups.') !!}
-            {!! Form::select('parameters', $parameters, old('parameters'), ['class' => 'form-control', 'id' => 'parameter']) !!}
         </div>
     </div>
 
