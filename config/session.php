8652e3e079ebf20685ef2a38fe2eb7a79ad69626
Toffee-tama <65995905+Toffee-tama@users.noreply.github.com>
2021-02-17 09:14:22 +0800

    no message

--- config/session.php
+++ config/session.php
@@ -153,7 +153,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', 'http://localhost/'),
+    'domain' => env('SESSION_DOMAIN', 'beanling-patch.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -166,7 +166,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', false),
+    'secure' => env('SESSION_SECURE_COOKIE', true),
 
     /*
     |--------------------------------------------------------------------------
