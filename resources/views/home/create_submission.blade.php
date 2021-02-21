525721c33d2b92d5fb95449814fa78cbd76d26e5
Toffee-tama <65995905+Toffee-tama@users.noreply.github.com>
2021-02-21 12:44:18 +0800

    no message

--- resources/views/home/create_submission.blade.php
+++ resources/views/home/create_submission.blade.php
@@ -88,9 +88,9 @@
 
     @include('widgets._character_select', ['characterCurrencies' => $characterCurrencies])
     @if($isClaim)
-        @include('widgets._loot_select_row', ['items' => $items, 'currencies' => $currencies, 'pets' => $pets, 'showLootTables' => false, 'showRaffles' => true, 'showRecipes' => true])
+        @include('widgets._loot_select_row', ['items' => $items, 'currencies' => $currencies, 'showLootTables' => false, 'showRaffles' => true])
     @else
-        @include('widgets._loot_select_row', ['items' => $items, 'currencies' => $currencies, 'pets' => $pets, 'showLootTables' => false, 'showRaffles' => false, 'showRecipes' => false])
+        @include('widgets._loot_select_row', ['items' => $items, 'currencies' => $currencies, 'showLootTables' => false, 'showRaffles' => false])
     @endif
 
     <div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog">
@@ -116,9 +116,9 @@
 @parent
 @if(!$closed)
     @if($isClaim)
-        @include('js._loot_js', ['showLootTables' => false, 'showRaffles' => true, 'showRecipes' => true])
+        @include('js._loot_js', ['showLootTables' => false, 'showRaffles' => true])
     @else
-        @include('js._loot_js', ['showLootTables' => false, 'showRaffles' => false, 'showRecipes' => false])
+        @include('js._loot_js', ['showLootTables' => false, 'showRaffles' => false])
     @endif
     @include('js._character_select_js')
     @include('widgets._inventory_select_js', ['readOnly' => true])
@@ -131,22 +131,18 @@
             var $confirmationModal = $('#confirmationModal');
             var $formSubmit = $('#formSubmit');
             var $submissionForm = $('#submissionForm');
-
             @if(!$isClaim)
                 var $prompt = $('#prompt');
                 var $rewards = $('#rewards');
-
                 $prompt.selectize();
                 $prompt.on('change', function(e) {
                     $rewards.load('{{ url('submissions/new/prompt') }}/'+$(this).val());
                 });
             @endif
-
             $submitButton.on('click', function(e) {
                 e.preventDefault();
                 $confirmationModal.modal('show');
             });
-
             $formSubmit.on('click', function(e) {
                 e.preventDefault();
                 $submissionForm.submit();
