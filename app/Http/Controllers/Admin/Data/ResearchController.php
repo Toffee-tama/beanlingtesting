a983156e099065ee4d04f3593f46dab38c0491c1
Toffee-tama <65995905+Toffee-tama@users.noreply.github.com>
2021-04-04 20:35:21 +0800

    no message

--- app/Http/Controllers/Admin/Data/ResearchController.php
+++ app/Http/Controllers/Admin/Data/ResearchController.php
@@ -105,6 +105,7 @@ class ResearchController extends Controller
             'currencies' => Currency::where('is_user_owned', 1)->orderBy('name')->pluck('name', 'id'),
             'tables' => LootTable::orderBy('name')->pluck('name', 'id'),
             'raffles' => Raffle::where('rolled_at', null)->where('is_active', 1)->orderBy('name')->pluck('name', 'id'),
+            'recipes'=> Recipe::orderBy('name')->pluck('name', 'id'),
         ]);
     }
 
