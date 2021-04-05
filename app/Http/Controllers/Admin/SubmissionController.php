2ea755ff2e5b780a7f65eb2a40b8f772d2051ff4
Toffee-tama <65995905+Toffee-tama@users.noreply.github.com>
2021-04-05 22:55:59 +0800

    no message

--- app/Http/Controllers/Admin/SubmissionController.php
+++ app/Http/Controllers/Admin/SubmissionController.php
@@ -97,7 +97,7 @@ class SubmissionController extends Controller
             'raffles' => Raffle::where('rolled_at', null)->where('is_active', 1)->orderBy('name')->pluck('name', 'id'),
             'recipes'=> Recipe::orderBy('name')->pluck('name', 'id'),
             'pets' => Pet::orderBy('name')->pluck('name', 'id'),
-            'count' => Submission::where('prompt_id', $id)->where('status', 'Approved')->where('user_id', $submission->user_id)->count(),
+            'count' => $count,
             'prompt' => $prompt,
             'limit' => $limit
         ] : []));
