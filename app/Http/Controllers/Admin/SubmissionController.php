9cc9c0c38418076ad97c329e71172e0383a02fa4
Toffee-tama <65995905+Toffee-tama@users.noreply.github.com>
2021-04-05 09:26:35 +0800

    no message

--- app/Http/Controllers/Admin/SubmissionController.php
+++ app/Http/Controllers/Admin/SubmissionController.php
@@ -99,7 +99,7 @@ class SubmissionController extends Controller
             'tables' => LootTable::orderBy('name')->pluck('name', 'id'),
             'raffles' => Raffle::where('rolled_at', null)->where('is_active', 1)->orderBy('name')->pluck('name', 'id'),
             'recipes'=> Recipe::orderBy('name')->pluck('name', 'id'),
-            'count' => Submission::where('prompt_id', $id)->where('status', 'Approved')->where('user_id', $submission->user_id)->count(),
+            'count' => $count,
             'prompt' => $prompt,
             'limit' => $limit
         ] : []));
